90cda22b6691c13b9b5d6c8affd253a864a5c88b
Kadek Aditya Premaswara <101009483+Kleditz@users.noreply.github.com>
2023-02-04 20:29:03 +0800

    Update 🏡_Home.py

--- 🏡_Home.py
+++ 🏡_Home.py
@@ -72,7 +72,7 @@ markdown = """
 - User able to read the pipeline of our computation process at `Google Earth Pipeline Pages` on the side-bar.
 - To read the resource we used in this project, user may need to select `Resource Pages` on the side-bar.
 - User also able to see our Team, as what `Our Team Pages` on the side-bar contained.
-- To see the our code for this web-apps, you can visit our `GitHub Repository` at [here](https://github.com/Kleditz/MHI-geo-challenge).
+- To see our sourcecode for this web-apps, you can visit our `GitHub Repository` at [here](https://github.com/Kleditz/MHI-geo-challenge).
 
 `Current Version : v1.0.0`
 """
